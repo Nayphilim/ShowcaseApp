a9935541b34a800fc8d4f9197df4d7698580fd71
Nathan Balfour <nathan.balfour2018@my.ntu.ac.uk>
2021-02-18 02:18:23 +0000

    implemented user register system

--- app/src/main/java/com/nayphilim/showcaseapp/RegisterActivity.java
+++ app/src/main/java/com/nayphilim/showcaseapp/RegisterActivity.java
@@ -2,30 +2,147 @@ package com.nayphilim.showcaseapp;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText firstNameText,lastNameText,emailText,passwordText;
+    private Button registerButton;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
+
+        firstNameText = findViewById(R.id.firstNameRegister);
+        lastNameText = findViewById(R.id.lastNameRegister);
+        emailText = findViewById(R.id.emailRegister);
+        passwordText = findViewById(R.id.passwordRegister);
+
+        registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(this);
+
+        progressBar = findViewById(R.id.progressBar);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.registerButton:
+                registerUser();
+        }
+    }
+
+    private void registerUser() {
+        String firstName = firstNameText.getText().toString().trim();
+        String lastName = lastNameText.getText().toString().trim();
+        String email = emailText.getText().toString().trim();
+        String password = passwordText.getText().toString().trim();
+
+        //password should contain one capital, one number and one symbol
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$";
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Matcher matcher;
+
+        if(firstName.isEmpty()){
+            firstNameText.setError("First name is required");
+            firstNameText.requestFocus();
+            return;
+        }
+
+        if(lastName.isEmpty()){
+            lastNameText.setError("Last name is required");
+            lastNameText.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty()){
+            emailText.setError("Email is required");
+            emailText.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailText.setError("Please enter a valid email address");
+            emailText.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            passwordText.setError("Please create a password");
+            passwordText.requestFocus();
+            return;
+        }
+
+        if(!pattern.matcher(password).matches()){
+            passwordText.setError("Please create a valid password which contain at least one capital letter, number and special character");
+            passwordText.requestFocus();
+            return;
+        }
+
+        //establish connections to firebase and register user
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            User user = new User(firstName, lastName, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(RegisterActivity.this, "User has been registered successfully", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+
+                                    } else {
+                                        Toast.makeText(RegisterActivity.this, "Failed to register user, please try again", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+
+
+
+                        }
+                        else{
+                            Toast.makeText(RegisterActivity.this, "Failed to register user, please try again", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+
+                });
+
+
+
+
     }
 }
--- app/src/main/java/com/nayphilim/showcaseapp/SignInActivity.java
+++ app/src/main/java/com/nayphilim/showcaseapp/SignInActivity.java
@@ -2,13 +2,33 @@ package com.nayphilim.showcaseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
 
+    private TextView registerLink;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        registerLink = findViewById(R.id.register);
+
+        mAuth = FirebaseAuth.getInstance();
+        
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this,RegisterActivity.class );
+                startActivity(intent);
+            }
+        });
     }
+
 }
